132f600df08c535cee6a9a88962df42aa0d4e5a3
Sharath <88310969+sharath-io@users.noreply.github.com>
2022-10-24 21:03:10 +0000

    feat: added options for the user

--- index.js
+++ index.js
@@ -3,49 +3,62 @@ var score = 0;
 
 const questions = [
   {
-    question: "Who vanished first, in infinity war ",
-    answer: "Bucky Barnes",
+    question: 'Who vanished first, in infinity war ',
+    options: 'A:BuckyBarnes     B:NickFury     C:StephenStrange     D:TonyStark ',
+    answer: 'A',
   },
   {
-    question: "Who vanished last, in infinity war ",
-    answer: "Nick Fury",
+    question: 'Who vanished last, in infinity war ',
+    options: 'A:SamWilson     B:BruceBanner     C:SteeveRogers     D:NickFury ',
+    answer: 'D',
   },
   {
-    question: "Who knows what exactly happens in infinity war among heroes ",
-    answer: "Stephen Strange",
+    question: 'Who knows what exactly happens in infinity war among heroes ',
+    options: 'A:TonyStark     B:NickFury     C:StephenStrange     D:BuckyBarnes ',
+    answer: 'C',
   },
   {
-    question: "What is the first Infinity Stone thanos got ",
-    answer: "Power stone",
+    question: 'What is the first Infinity Stone thanos got ',
+    options: 'A:PowerStone     B:MindStone     C:SpaceStone     D:SoulStone ',
+    answer: 'A',
   },
   {
-    question: "What is the last Infinity Stone thanos got ",
-    answer: "Mind stone",
+    question: 'What is the last Infinity Stone thanos got ',
+    options: 'A:PowerStone     B:MindStone     C:SpaceStone     D:SoulStone ',
+    answer: 'B',
   },
   {
-    question: "Who did first snap in end game ",
-    answer: "Bruce Banner",
+    question: 'Who did first snap in end game ',
+    options: 'A:TonyStark     B:NickFury     C:BuckyBarnes     D:BruceBanner ',
+    answer: 'D',
   },
   {
-    question: "Who did last snap in end game ",
-    answer: "Tony Stark",
+    question: 'Who did last snap in end game ',
+    options: 'A:BuckyBarnes    B:NickFury     C:TonyStark     D:StephenStrange ',
+    answer: 'C',
   },
   {
-    question: "Who is the first one to came back after vanishing in infinity war ",
-    answer: "Sam Wilson",
+    question: 'Who is the first one to came back after vanishing in infinity war ',
+    options: 'A:SamWilson    B:BuckyBarnes     C:SteveRogers     D:StephenStrange ',
+    answer: 'A',
   },
   {
-    question: "Who has the power to take Thor's hammer other than Thor ",
-    answer: "Steve Rogers",
+    question: 'Who has the power to take Thors hammer other than Thor ',
+    options: 'A:BruceBanner    B:BuckyBarnes     C:SteveRogers     D:StephenStrange ',
+    answer: 'C',
   },
   {
-    question: "Whom do we love 3000 ",
-    answer: "Tony Stark",
+    question: 'Whom do we love 3000 ',
+    options: 'A:TonyStark    B:BuckyBarnes     C:SteveRogers     D:StephenStrange ',
+    answer: 'A',
   },
 ];
 
-function quiz(question, answer) {
-  var userAnswer = read.question(question);
+
+
+function quiz(question, options, answer) {
+  console.log(question);
+  var userAnswer = read.question(options + ' \n');
   if (userAnswer === answer) {
     score = score + 1;
     console.log('right ');
@@ -57,7 +70,7 @@ function quiz(question, answer) {
 function call_quiz() {
   for (var i = 0; i < questions.length; i++) {
     console.log('--------------');
-    quiz(questions[i].question, questions[i].answer);
+    quiz(questions[i].question, questions[i].options, questions[i].answer);
   }
 }
 
@@ -65,7 +78,7 @@ function play() {
   const userName = read.question('Whats your name ? ');
   console.log('Hello ' + userName + ' Welcome to Avengers Quiz' + ' \nAnswer these in  character names not super hero names ');
   call_quiz();
-  console.log('Your total score is '+ score);
+  console.log('Your total score is ' + score);
 }
 
 play();
