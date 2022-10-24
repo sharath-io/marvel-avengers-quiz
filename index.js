d13d58c1258dbc42b4dd58c5918deaa05fb67ca0
Sharath <88310969+sharath-io@users.noreply.github.com>
2022-10-24 07:15:51 +0000

    added questions

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 const read = require('readline-sync');
 const userName = read.question('Whats your name ? ');
-console.log('Hello ' + userName + ' Welcome to Avengers Quiz')
+console.log('Hello ' + userName + ' Welcome to Avengers Quiz'+ ' \nAnswer these in character names not super hero names ')
+
+const questions =[
+  {
+    question: "Who vanished first, in infinity war ",
+    answer: "Bucky Barnes",
+  },
+  {
+    question: "Who vanished last, in infinity war ",
+    answer: "Nick Fury",
+  },
+  {
+    question: "Who knows what exactly happens in infinity war among heroes ",
+    answer: "Stephen Strange",
+  },
+  {
+    question: "What is the first Infinity Stone thanos got ",
+    answer: "Power stone",
+  },
+  {
+    question: "What is the last Infinity Stone thanos got ",
+    answer: "Mind stone",
+  },
+  {
+    question: "Who did first snap in end game ",
+    answer: "Bruce Banner",
+  },
+  {
+    question: "Who did last snap in end game ",
+    answer: "Tony Stark",
+  },
+  {
+    question: "Who is the first one to came back after vanishing in infinity war ",
+    answer: "Sam Wilson",
+  },
+  {
+    question: "Who has the power to take Thor's hammer other than Thor ",
+    answer: "Steve Rogers",
+  },
+  {
+    question: "Whom do we love 3000 ",
+    answer: "Tony Stark",
+  },
+]
